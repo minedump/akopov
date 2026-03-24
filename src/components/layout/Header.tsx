0951075fa4a97a091ece1ea47115b318e59eef23
Vlod <103286495+daphysnotundersanctions@users.noreply.github.com>
2026-03-24 21:39:38 +0300

    fix

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,18 +1,47 @@
 "use client";
 import Link from "next/link";
 import React, { useEffect, useState } from "react";
-import { createPortal } from "react-dom";
+import reactDom from "react-dom";
+
 import { Cross as Hamburger } from "hamburger-react";
 
 export const Header: React.FC = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   useEffect(() => {
-    if (!document) return;
     isMenuOpen
       ? (document.body.style.overflow = "hidden")
       : (document.body.style.overflow = "auto");
   });
+
+  const sideBar = (
+    <div
+      className={`${isMenuOpen ? "block" : "hidden"} fixed z-[3] top-0 right-0 w-screen h-full backdrop-blur-sm md:max-w-[345px] p-6 md:p-16 bg-[#121212E5]`}
+    >
+      <div className="flex flex-col items-end">
+        <Hamburger
+          toggled={isMenuOpen}
+          color="white"
+          onToggle={() => setIsMenuOpen(!isMenuOpen)}
+        />
+        <div className="text-white text-end mt-20 ">
+          <p className="mb-6 text-[12px]">разделы портфолио</p>
+
+          <div className="flex text-xl font-bold flex-col gap-2">
+            <Link href="/retail" onClick={() => setIsMenuOpen(false)}>
+              <p>ритейл</p>
+            </Link>
+            <Link href="/interiors" onClick={() => setIsMenuOpen(false)}>
+              <p>жилые интерьеры</p>
+            </Link>
+            <Link href="/graphics" onClick={() => setIsMenuOpen(false)}>
+              <p>графический дизайн</p>
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 
   return (
     <header className="bg-transparent z-[2] fixed w-full">
@@ -29,35 +58,9 @@ export const Header: React.FC = () => {
           />
         </div>
       </nav>
-      {createPortal(
-        <div
-          className={`${isMenuOpen ? "block" : "hidden"} fixed z-[3] top-0 right-0 w-screen h-full backdrop-blur-sm md:max-w-[345px] p-6 md:p-16 bg-[#121212E5]`}
-        >
-          <div className="flex flex-col items-end">
-            <Hamburger
-              toggled={isMenuOpen}
-              color="white"
-              onToggle={() => setIsMenuOpen(!isMenuOpen)}
-            />
-            <div className="text-white text-end mt-20 ">
-              <p className="mb-6 text-[12px]">разделы портфолио</p>
 
-              <div className="flex text-xl font-bold flex-col gap-2">
-                <Link href="/retail" onClick={() => setIsMenuOpen(false)}>
-                  <p>ритейл</p>
-                </Link>
-                <Link href="/interiors" onClick={() => setIsMenuOpen(false)}>
-                  <p>жилые интерьеры</p>
-                </Link>
-                <Link href="/graphics" onClick={() => setIsMenuOpen(false)}>
-                  <p>графический дизайн</p>
-                </Link>
-              </div>
-            </div>
-          </div>
-        </div>,
-        document.body,
-      )}
+      {typeof window === "object" &&
+        reactDom.createPortal(sideBar, document.body)}
     </header>
   );
 };
